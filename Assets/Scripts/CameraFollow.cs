bf53485c58b1be0c272a2ddca5422dab2f7e2ab9
aluno <aluno@udicentro.local>
2021-11-22 09:06:14 -0300

    estruturação 2a fase (deserto)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float maximoX;
+    [SerializeField] private float minimoX;
+    [SerializeField] private float maximoY;
+    [SerializeField] private float minimoY;
+
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, minimoX, maximoX),Mathf.Clamp(player.position.y, minimoY, maximoY),-10);
+    }
+}
