2fa973b9d020f3a17563e3d67656371f6f01cb7d
aluno <aluno@udicentro.local>
2021-11-29 15:24:56 -0300

    testes contagem de itens sem DB

--- Assets/Scripts/MapaController.cs
+++ Assets/Scripts/MapaController.cs
@@ -10,25 +10,21 @@ public class MapaController : MonoBehaviour
     public void GetScene_Caverna ()
     {
         StartCoroutine(LoadLevel("lv_caverna"));
-        //SceneManager.LoadScene("lv_caverna", LoadSceneMode.Single);
     }
 
     public void GetScene_Deserto ()
     {
         StartCoroutine(LoadLevel("lv_deserto"));
-        //SceneManager.LoadScene("lv_deserto", LoadSceneMode.Single);
     }
 
     public void GetScene_Floresta ()
     {
         StartCoroutine(LoadLevel("lv_floresta"));
-        //SceneManager.LoadScene("lv_floresta", LoadSceneMode.Single);
     }
 
     public void GetScene_Cemiterio ()
     {
         StartCoroutine(LoadLevel("lv_cemiterio"));
-        //SceneManager.LoadScene("lv_cemiterio", LoadSceneMode.Single);
     }
 
     IEnumerator LoadLevel (string levelName)
